8d95470ed9b1bc075284a854aa3997a45301701f
Pradyumna Paddu <97731439+PADDU-MTKF@users.noreply.github.com>
2023-05-11 17:05:43 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD START"
-python3 -m pip3 install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+python -m pip3 install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo " BUILD END"
